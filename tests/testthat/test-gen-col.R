f603a71e104dd29c35f205dcf0cee1e1e607fb55
Benjamin J. Cairns <ben.cairns@ndph.ox.ac.uk>
2019-10-16 15:13:18 +0100

    Add further tests for types and edge cases

--- tests/testthat/test-gen-col.R
+++ tests/testthat/test-gen-col.R
@@ -1,5 +1,24 @@
 context("gen_col()")
 
 test_that("gen_col returns expected classes", {
-  expect_equal(class(gen_col(as.integer(c()), 1)), "integer")
+  expect_true(is.integer(gen_col(as.integer(c()), 10)))      # integer
+  expect_true(is.numeric(gen_col(as.numeric(c()), 10)))      # numeric
+  expect_true(is.list(gen_col(as.list(c()), 10)))            # list
+  expect_true(is.factor(gen_col(as.factor(c()), 10)))        # factor
+  expect_true(is.logical(gen_col(TRUE, 10)))                 # logical
+  expect_identical(                                          # date
+    "Date",
+    class(gen_col(as.Date(character()), 10))
+  )
+  expect_identical(                                          # date-time
+    "POSIXct",
+    class(gen_col(as.POSIXct(numeric(), origin = "1970-01-01"), 10))[1]
+  )
+})
+
+test_that("gen_col can return a length-0 vector", {
+  expect_identical(
+    length(gen_col(as.integer(c()), 0)),
+    0L
+  )
 })
--- tests/testthat/test-stubblise.R
+++ tests/testthat/test-stubblise.R
@@ -9,3 +9,22 @@ test_that("stubblise maintains structure", {
   expect_identical(lapply(mtcars, class), lapply(stubblise(mtcars), class))
 
 })
+
+test_that("stubblise can return a tibble with 0 rows",{
+
+  iris_stbl <- stubblise(iris, rows = 0)
+
+  expect_identical(lapply(iris, names), lapply(iris_stbl, names))
+  expect_identical(lapply(iris, class), lapply(iris_stbl, class))
+
+  expect_true(
+    dim(iris_stbl)[1] == 0L
+  )
+
+})
+
+test_that("stubblise and stubblize are equivalent", {
+
+  expect_identical(stubblise, stubblize)
+
+})
